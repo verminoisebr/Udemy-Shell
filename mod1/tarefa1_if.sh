77a6fd47deea4beab98410d5915bf2f860f53dce
Marcelo Boschi <marceloboschi78@hotmail.com>
2019-06-19 09:10:01 -0300

    Chapter 1 finished

--- mod1/tarefa1_if.sh
+++ mod1/tarefa1_if.sh
@@ -7,3 +7,11 @@
 #else
 #  echo "Não é maior que 10..."
 #  echo "Parâmetro foi: $1"
+
+
+: '
+Neste pequeno exercício, vamos validar um pouco do conhecimento adquirido em relação às variáveis reservadas, bem como a correta utilização da condicional If.
+Então, escreva um código que faça o seguinte:
+Validar se o primeiro parâmetro enviado é maior que 10
+Se for maior, mostre uma mensagem na tela com o nome do script e seu respectivo PID de execução
+'
